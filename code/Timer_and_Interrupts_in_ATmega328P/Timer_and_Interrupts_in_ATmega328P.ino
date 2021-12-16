d7b02dec10b840e96658935673421e857fe74483
Sujal <vasoyasujal42@gmail.com>
2021-12-16 21:46:25 +0530

    final code with one problem
      delay(0.001);
     if I not use this when press push button one time loop will rotate two times or three time and many times more rotating.

--- code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
+++ code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
@@ -1,18 +1,26 @@
 // teammates name sujal chintan darshnik
 // project description:- learning timer interrupt
-// blink 8 led in sequence pin 2-9 
+// blink 8 led in sequence with push button pin 2-9 
 void setup() {
   DDRD = B11111100; 
   DDRB = B00000011;
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{ int READ;
+  PORTB = B00001000;
+  delay(0.001);   // -- issue 
+  READ = (PINB >> 3 & B00001000 >> 3); // read B3 = pin 10
+  Serial.println(READ);
+  if(READ==0)
+  {
   PORTD |=  B00000100;   // Add OR logical expression for add bit
   delay(100);
   PORTD &= !B00000100;   // Add AND logical expression for remove bit
   delay(100);
-
-  PORTD = B00001000;   
+  
+  PORTD = B00001000;     
   delay(100);
   cl();
   PORTD = B00010000;   
@@ -33,6 +41,7 @@ void loop() {
   PORTB = B00000010;   
   delay(100);
   cl();
+  }
 }
 
 void cl()
