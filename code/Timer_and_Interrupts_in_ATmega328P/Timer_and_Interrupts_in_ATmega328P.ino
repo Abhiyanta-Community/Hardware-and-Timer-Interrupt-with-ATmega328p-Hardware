b2de910dc6b1aa851762428a1724094a14bfe26a
Sujal <vasoyasujal42@gmail.com>
2021-12-17 20:04:51 +0530

    blink led in sequence with push button (problem)

--- code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
+++ code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
@@ -1,51 +1,56 @@
 // teammates name sujal chintan darshnik
 // project description:- learning timer interrupt
-// blink 8 led in sequence with push button pin 2-9 
+// blink 9 (pin 2-10) led in sequence with push button (pin 11)  
 void setup() {
-  DDRD = B11111100; 
-  DDRB = B00000011;
+  DDRD = B11111100; // set direction of registor
+  DDRB = B00000111; // set direction of registor
   Serial.begin(9600);
 }
 
 void loop() 
 { int READ;
-  PORTB = B00001000;
-  delay(0.001);   // -- issue 
+  PORTB = B00001000; // pull-up registor
   READ = (PINB >> 3 & B00001000 >> 3); // read B3 = pin 10
   Serial.println(READ);
-  if(READ==0)
-  {
-  PORTD |=  B00000100;   // Add OR logical expression for add bit
-  delay(100);
-  PORTD &= !B00000100;   // Add AND logical expression for remove bit
-  delay(100);
+  if(READ == 0)
+  {   
+    PORTD |=  B00000100;   // Add OR logical expression for add bit
+    delay(100);
+    PORTD &= !B00000100;   // Add AND logical expression for remove bit
+    delay(100);
   
-  PORTD = B00001000;     
-  delay(100);
-  cl();
-  PORTD = B00010000;   
-  delay(100);
-  cl();
-  PORTD = B00100000;   
-  delay(100);
-  cl();
-  PORTD = B01000000;   
-  delay(100);
-  cl();
-  PORTD = B10000000;
-  delay(100);
-  cl();
-  PORTB = B00000001;   
-  delay(100);
-  cl();
-  PORTB = B00000010;   
-  delay(100);
-  cl();
-  }
+    PORTD = B00001000;     
+    delay(100);
+    cl();
+    PORTD = B00010000;   
+    delay(100);
+    cl();
+    PORTD = B00100000;   
+    delay(100);
+    cl();
+    PORTD = B01000000;   
+    delay(100);
+    cl();
+    PORTD = B10000000;
+    delay(100);
+    cl();
+    PORTB = B00000001;   
+    delay(100);
+    cl();
+    PORTB = B00000010;   
+    delay(100);
+    cl();
+    PORTB = B00000100;   
+    delay(100);
+    cl();
+  } 
 }
-
+//////////////////////////////////
+//function name: cl()
+//description: for clear the bits
+//////////////////////////////////
 void cl()
 {
-PORTD = B00000000;
-PORTB = B00000000;
+PORTD &= !B11111111;
+PORTB &= !B00000111;
 }
