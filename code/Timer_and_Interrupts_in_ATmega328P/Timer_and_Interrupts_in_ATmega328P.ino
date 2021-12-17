b163d62c7f9917a0e457f3485002beef10bba6b6
Sujal <vasoyasujal42@gmail.com>
2021-12-17 20:09:53 +0530

    blink led in sequence with push button
    final code with no error

--- code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
+++ code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
@@ -1,15 +1,15 @@
 // teammates name sujal chintan darshnik
 // project description:- learning timer interrupt
-// blink 9 (pin 2-10) led in sequence with push button (pin 11)  
+// blink 9 (pin 2-10) led in sequence with push button (pin 11) no added any pull up registor externally
 void setup() {
   DDRD = B11111100; // set direction of registor
   DDRB = B00000111; // set direction of registor
+  PORTB = B00001000; // pull-up registor
   Serial.begin(9600);
 }
 
 void loop() 
 { int READ;
-  PORTB = B00001000; // pull-up registor
   READ = (PINB >> 3 & B00001000 >> 3); // read B3 = pin 10
   Serial.println(READ);
   if(READ == 0)
@@ -43,7 +43,10 @@ void loop()
     PORTB = B00000100;   
     delay(100);
     cl();
+    Serial.println(PINB);
   } 
+  PORTB = B00001000; // pull-up registor
+  Serial.println(PINB);
 }
 //////////////////////////////////
 //function name: cl()
