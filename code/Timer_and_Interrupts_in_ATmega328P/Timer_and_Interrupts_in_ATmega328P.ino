bd830e9adffead598896585fe98728073992accd
Sujal <vasoyasujal42@gmail.com>
2021-12-19 00:22:12 +0530

    Final code
    many changes done in code adding loop for rotating led and remove function

--- code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
+++ code/Timer_and_Interrupts_in_ATmega328P/Timer_and_Interrupts_in_ATmega328P.ino
@@ -4,54 +4,27 @@
 void setup() {
   DDRD = B11111100; // set direction of registor
   DDRB = B00000111; // set direction of registor
-  PORTB = B00001000; // pull-up registor
-  
 }
 
 void loop() 
-{ int READ;
-  PORTB = B00001000; // pull-up registor
+{ 
+  int READ; // veriable
+  PORTB = B00001000; //pull-up register
   READ = (PINB >> 3 & B00001000 >> 3); // read B3 = pin 10
-  Serial.println(READ);
-  if(READ == 0)
-  {   
-    PORTD |=  B00000100;   // Add OR logical expression for add bit
-    delay(100);
-    PORTD &= !B00000100;   // Add AND logical expression for remove bit
-    delay(100);
   
-    PORTD = B00001000;     
-    delay(100);
-    cl();
-    PORTD = B00010000;   
-    delay(100);
-    cl();
-    PORTD = B00100000;   
-    delay(100);
-    cl();
-    PORTD = B01000000;   
-    delay(100);
-    cl();
-    PORTD = B10000000;
-    delay(100);
-    cl();
-    PORTB = B00001001;   
-    delay(100);
-    cl();
-    PORTB = B00001010;   
-    delay(100);
-    cl();
-    PORTB = B00001100;   
-    delay(100);
-    cl();  
-  } 
-}
-//////////////////////////////////
-//function name: cl()
-//description: for clear the bits
-//////////////////////////////////
-void cl()
-{
-PORTD &= !B11111111;
-PORTB &= !B00000111;
-}
+  if(READ == 0)
+  { 
+    for(int i=1;i<=6;i++)
+    {
+     PORTD = B0000010 << i;   // its shigting 1 right to left
+     delay(100);
+     PORTD &= !B11111111;     // its clearing the bit for led off 
+    }  
+    for(int j=0;j<=2;j++)
+    { 
+     PORTB = B00001000 | B00000001 << j; //its shigting 1 right to left this should not effected on 4th bit becouse its for button
+     delay(100);
+     PORTB &= !B00000111;    // its clearing the bit for led off but should "not effect on any other bits" 
+    }  
+  }  
+ }
