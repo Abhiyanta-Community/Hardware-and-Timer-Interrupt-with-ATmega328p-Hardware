72a09b17169e520b2cd5941b11d26f1147004eed
Sujal <vasoyasujal42@gmail.com>
2021-12-23 09:28:19 +0530

    final code with update

--- code/Implementing_Hardware_Interrupts/Implementing_Hardware_Interrupts.ino
+++ code/Implementing_Hardware_Interrupts/Implementing_Hardware_Interrupts.ino
@@ -27,32 +27,24 @@ void setup() {
 }
  
 void loop() 
-{ int READ;
-  READ = (PIND >> 2 & B00000100 >> 2); // Read B3 = pin 2    
-    if(READ == 0)
-    { 
-      for(i=0;i<=8;i++)
-      {   // if we not apply two separate IF then when interrupt is generate if we in first if condition then not any 
-          // problem occurs but if we in secound loop and interrupt is generate then it's not start from first its stat
-          // from 6th led or loop rotating in PORTD resister.
-        if(i <= 4) // when the loop rotating first PORTD pins control by this IF statment
-        {          
-        PORTD |= (1 << i+3);  //i+3 because bit start from 3
-        delay(200);
-        PORTD &= ~B11111000;  // clear the bits
-        }
-        if(i>=5 && i<=8) // when the loop rotating first PORTB pins control by this IF statment
-        {
-        PORTB |= (1 << i-5);  // i-5 because bit start from 0
-        delay(200);
-        PORTB &= ~B00001111;  // clear the bits
-        }
-      }  
-    }
+{ 
 }
 
 ISR(INT0_vect) //Interrupt Service Routine 
 {  
-  i=0;
-  j=0;
+   for(i=0;i<=8;i++)
+   { 
+      if(i <= 4) // when the loop rotating first PORTD pins control by this IF statment
+      {          
+      PORTD |= (1 << i+3);  //i+3 because bit start from 3
+      delay(10000);
+      PORTD &= ~B11111000;  // clear the bits
+      }
+      if(i>=5 && i<=8) // when the loop rotating first PORTB pins control by this IF statment
+      {
+      PORTB |= (1 << i-5);  // i-5 because bit start from 0
+      delay(10000);
+      PORTB &= ~B00001111;  // clear the bits
+      }
+    }  
 }
