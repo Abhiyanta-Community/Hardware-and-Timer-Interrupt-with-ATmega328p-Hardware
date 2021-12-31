072ff2936a700c73f220501c784cd5035420c833
Sujal <vasoyasujal42@gmail.com>
2021-12-31 22:36:13 +0530

    Led rolling with timer interrupt

--- code/Implementing-Timer-Interrupts/Implementing-Timer-Interrupts.ino
+++ code/Implementing-Timer-Interrupts/Implementing-Timer-Interrupts.ino
@@ -1,12 +1,13 @@
-const int led = PB5;
+
+int i=0;
 
 //counter and comper value
 const uint16_t t1_load = 0;
-const uint16_t t1_comp = 6250;// time 100ms
+const uint16_t t1_comp = 10000;// time 100ms=6250
 
 void setup() {
-   DDRB |= (1<<led);
-   
+   DDRB = B00011111;
+   DDRD = B11111100;
    //reset Timer counter Reg A 
    TCCR1A = 0;
 
@@ -29,7 +30,26 @@ void loop() {
  }
 
 ISR(TIMER1_COMPA_vect)
-{
+{  PORTD = B00000000;
+   PORTB = B00000000;
    TCNT1 = t1_load;
-   PORTB ^= (1<<led); 
+   
+   led(); 
+}
+
+void led()
+{    i++;
+      if(i <= 6) // when the loop rotating first PORTD pins control by this IF statment
+      {          
+      PORTD |= (1 << i+1);  //i+3 because bit start from 3
+      }
+      if(i>=6 && i<=10) // when the loop rotating first PORTB pins control by this IF statment
+      {
+      PORTB |= (1 << i-6);  // i-5 because bit start from 0
+      }
+    
+    if(i==10)
+    {
+      i=0;
+    }
 }
